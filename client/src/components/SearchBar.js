96bab8447b2d309a9d80c087a50352ae0286f076
agomez <agomez@mcclatchyservices.com>
2021-01-06 21:29:34 -0500

    Created search bar view

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,23 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class SearchBar extends Component {
-  constructor() {
-    super();
-    this.state = {
-      items: []
-    }
+const SearchBar = () => {
+
+  const [search, setSearch] = useState('');
+
+  const onChange = (text) => {
+    setSearch(text);
+  };
+
+  const onKeyPress = (event) => {
+    const code = event.keyCode || event.which;
+    if(code === 13) {
+      console.log('hola enter');
+    } 
+  };
+
+  const getResults = () => {
+    console.log('soy el metodo magico');
   }
 
-  componentDidMount() {
-    fetch('/api/items?q=patineta')
-      .then(res => res.json())
-      .then(items => this.setState({items: items.items}, () => console.log('Fetched', items)))
-      .catch(err => console.log(err));
-  }
- 
-  render () {
-    return <div><h1>HOLA MUNDO</h1></div>;
-  }
+  return (
+    <header className="search-bar">
+      <div className="wrapper">
+        <a className="nav-logo" href="/">Mercado Libre Colombia - Donde comprar y vender de todo</a>
+        <div className="search-form">
+          <input 
+            className="search-input"
+            type="text"
+            placeholder="Nunca dejes de buscar"
+            autoFocus
+            onChange ={(e) => onChange(e.target.value)}
+            onKeyPress={(e) => onKeyPress(e)}/>
+            <button className="search-button" onClick={getResults}>
+              <i className="fas fa-search"></i>
+            </button>
+        </div> 
+       </div>
+    </header>
+  );
 }
 
 export default SearchBar;
